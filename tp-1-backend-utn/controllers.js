ee17c0eee50925489808b0203503d6f76cf0872e
danyel salazar <danygasia2@gmail.com>
2026-04-24 00:11:04 -0300

    add: agtregue get por email de usuario

--- tp-1-backend-utn/controllers.js
+++ tp-1-backend-utn/controllers.js
@@ -1,6 +1,9 @@
+import { log } from "console";
 import { db } from "./config.js";
 import crypto from "crypto"
 
+
+// creamos los usuarios
 const adduser = async (username, email, password) =>{
     try {
         // validaciones:
@@ -11,6 +14,7 @@ const adduser = async (username, email, password) =>{
         if(!email.endsWith("@gmail.com")){
             throw new Error("El mail debe ser @gmail.com")
         }
+
         if(password.length < 4){
             throw new Error("La contrasenia debe tener al menos 4 caracteres")
         }
@@ -18,16 +22,16 @@ const adduser = async (username, email, password) =>{
         // generamos un id unico
         const id = crypto.randomUUID();
 
-        // insertamos el usuario en la base de datos:
 
         // primero creamos la query
         const query = `
             INSERT INTO users (id, username, email, password)
             VALUES (?, ?, ?, ?);`;
         
+        // insertamos el usuario en la base de datos:
         await db.query(query, [id, username, email, password] );
 
-        console.log("✅ Usuario creado");
+        console.log("✅Usuario creado");
 
         // cierro la conexion de la base de datos
         await db.end(); 
@@ -39,4 +43,32 @@ const adduser = async (username, email, password) =>{
     }
 }
 
-export {adduser}
+// traemos usuario por correo email
+const getUserByEmail = async(email)=>{
+    try {
+        const query = `
+            SELECT * FROM users
+            WHERE email LIKE ?;`
+
+        const [rows] = await db.query(query, `%${email}%`);
+       
+        if(rows.length <= 0){
+            return console.log('No se encontrom el usuario');
+        }
+
+        console.log(rows)
+
+        db.end()
+
+        return;
+        
+
+    } catch (error) {
+        console.error("Error: ", error);
+        
+    }
+}
+
+
+
+export {adduser, getUserByEmail}
--- tp-1-backend-utn/index.js
+++ tp-1-backend-utn/index.js
@@ -1,16 +1,51 @@
-import { adduser } from "./controllers.js";
+import { adduser , getUserByEmail} from "./controllers.js";
 
 const args = process.argv
-const datos = args.slice(2)
+const datos = args.slice(3)
+const operacion = args[2]
 
 
 const main = async ()=>{
 
-    await adduser(...datos)
+    // console.log("operacion:", operacion);
+    // console.log("Datos: ", datos);
+
+    switch(operacion){
+        case "add":
+            await adduser(...datos)
+            break;
+        case "getByEmail":
+            await getUserByEmail(...datos)
+            break;
+        default:
+            console.log(`
+==================================================
+  🚀  SISTEMA DE GESTIÓN DE USUARIOS - UTN
+==================================================
+
+📝 MODO DE USO:
+   npm run dev <operación> [argumentos...]
+
+📌 OPERACIONES:
+   ➕ add         -> npm run dev  add Danyel danyel@gmail.com 1234
+   🔍 getByUser  -> npm run dev  getByUser danyel@gmail.com
+   📋 getUsers   -> npm run dev  getUsers
+
+💡 Tip: Usa "comillas" para textos con espacios.
+==================================================
+`);
+            break;
+            
+    }
+
+
 
     // cierro el proceso de node asi puedo seguir escribiendo
     process.exit()
     // console.log(...datos);
+
+    
+    
     
 }
 
